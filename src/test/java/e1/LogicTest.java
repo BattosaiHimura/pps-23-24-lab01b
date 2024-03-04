1aca0134740ada350c3adc301e2c3aa6ac829ac4
nzuluaga <nikolay.zuluaga@studio.unibo.it>
2024-03-05 00:38:20 +0100

    added test suite for Logics interface

--- src/test/java/e1/LogicTest.java
+++ src/test/java/e1/LogicTest.java
@@ -3,13 +3,78 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 public class LogicTest {
 
+  Logics logics;
+  private final int SIZE = 5;
+  
+  @BeforeEach
+  void BeforeEach() {
+    Pair<Integer, Integer> pawn = new Pair<Integer,Integer>(3, 1);
+    Pair<Integer, Integer> knight = new Pair<Integer,Integer>(4, 3);
+    this.logics = new LogicsImpl(SIZE, pawn, knight);
+  }
+
   @Test
-  public void test() {
-    assert(true);
-    // TODO: Add your test logic here
-    // You can generate random inputs and assert the expected output
-    // For example:
-    // int result = Logic.someMethod(5, 10);
-    // assertEquals(expectedResult, result);
+  void testHasPawnOnPosition() {
+    assertTrue(this.logics.hasPawn(3, 1));
+  }
+
+  @Test
+  void testHasNotPawnOnPosition() {
+    assertTrue(this.logics.hasPawn(0, 0));
+  }
+
+  @Test
+  void testHasKnightOnPosition() {
+    assertTrue(this.logics.hasKnight(4, 3));
+  }
+
+  @Test
+  void testHasNotKnightOnPosition() {
+    assertFalse(this.logics.hasKnight(0, 0));
+  }
+
+  @Test
+  void hitXIndexMustBeWithinRange() {
+    assertThrows(IndexOutOfBoundsException.class, () -> this.logics.hit(SIZE, 0));
+  }
+
+  @Test
+  void hitYIntexMustBeWithinRange() {
+    assertThrows(IndexOutOfBoundsException.class, () -> this.logics.hit(0, SIZE));
+  }
+
+  @Test
+  void hitXIndexMustBePositive() {
+    assertThrows(IndexOutOfBoundsException.class, () -> this.logics.hit(-1, 0));
+  }
+
+  @Test
+  void hitYIndexMustBePositive() {
+    assertThrows(IndexOutOfBoundsException.class, () -> this.logics.hit(0, -1));
+  }
+
+  @Test
+  void testValidKnightMove() {
+    assertFalse(this.logics.hit(3,2));
+    assertTrue(this.logics.hasKnight(2, 2));
+  }
+
+  @Test
+  void testInvalidKnightMove() {
+    assertFalse(this.logics.hit(3,3));
+    assertFalse(this.logics.hasKnight(3, 3));
+    assertTrue(this.logics.hasKnight(4, 3));
+  }
+
+  @Test
+  void testValidPawnHit() {
+    assertTrue(this.logics.hit(3,1));
+  }
+
+  @Test
+  void testInvalidPawnHit() {
+    this.testValidKnightMove();
+    assertFalse(this.logics.hit(3,1));
+    assertTrue(this.logics.hasKnight(2, 2));
   }
 }
