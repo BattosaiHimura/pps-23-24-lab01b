f9545d5c18f086435a0cb6e3142f0ddbe84380f0
nzuluaga <nikolay.zuluaga@studio.unibo.it>
2024-03-10 13:40:59 +0100

    Implemented Playground according to previously prepared tests

---
+++ src/main/java/e2/playground/PlaygroundImpl.java
@@ -0,0 +1,40 @@
+package e2.playground;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import e2.Pair;
+import e2.layout.Cell;
+import e2.layout.CellImpl;
+import e2.layout.Grid;
+import e2.layout.GridImpl;
+import e2.strategies.PlacingStrategy;
+
+public class PlaygroundImpl implements Playground {
+
+    private Integer size;
+    private Grid grid;
+    private ArrayList<Cell> mines;
+
+    PlacingStrategy strategy;
+
+    public PlaygroundImpl(int size, PlacingStrategy strategy) {
+        this.size = size;
+        this.strategy = strategy;
+        this.grid = new GridImpl(this.size);
+        this.mines = new ArrayList<>();
+    }
+
+    @Override
+    public void setMines(int totalMines) {
+        for(Pair<Integer, Integer> pos : this.strategy.callStrategy(this.size)) {
+            this.mines.add(new CellImpl(pos));
+        }
+    }
+
+    @Override
+    public List<Cell> getMines() {
+        return this.mines;
+    }
+
+}
--- src/test/java/e2/playground/PlaygroundTest.java
+++ src/test/java/e2/playground/PlaygroundTest.java
@@ -5,13 +5,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import e2.strategies.PlacingStrategy;
+import e2.strategies.RandomPlacingStrategy;
+
 class PlaygroundTest {
 
     private Playground playground;
+    private static final int SIZE = 10;
+    private PlacingStrategy strategy;
 
     @BeforeEach
     void BeforeEach() {
-        this.playground = null;
+        this.strategy = new RandomPlacingStrategy();
+        this.playground = new PlaygroundImpl(SIZE, strategy);
     }
 
     @Test
@@ -20,7 +26,7 @@ class PlaygroundTest {
         int totalMines = 10;
 
         playground.setMines(totalMines);
-        assertEquals(playground.getMines().size(), totalMines);
+        assertEquals(totalMines, this.playground.getMines().size());
     }
 
 }
